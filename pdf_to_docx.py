266329776219aacecb609a0159b0b42aa4d1d4d1
Thiago Augusto França <thiagoaugusto.franca@hotmail.com>
2022-10-16 15:54:20 -0300

    Fix some issues in the code, now is it working properly

--- pdf_to_docx.py
+++ pdf_to_docx.py
@@ -1,10 +1,10 @@
 # pip install PDF2Docx
+# Se estiver usando a ultima versão do Python, talvez não funcione corretamente o pdf2docx, basta rodar o python em uma das versões anteriores 3.9, 3.8 .. por exemplo.
 from pdf2docx import parse
 import os
 
 # Lista os arquivos pdf na pasta e converte pra Word (Docx)
 for file in os.listdir(os.curdir):
     if file.endswith(".pdf"):
-        pdf_file = file
-        word_file = f"{pdf_file}.docx"
-        parse(pdf_file, word_file, start=0, end=None)
+        docxfile = file.replace(".pdf",".docx")
+        parse(file, docxfile, start=0, end=None)
